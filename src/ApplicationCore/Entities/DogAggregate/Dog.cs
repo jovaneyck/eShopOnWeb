5cc2f6c350ff4ed97fd3c4cba4e43a18d7a9a983
Jo Van Eyck <jovaneyck1988@hotmail.com>
2025-06-22 14:18:54 +0200

    add dog code

---
+++ src/ApplicationCore/Entities/DogAggregate/Dog.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public enum DogType
+{
+    POINTER,
+    RETRIEVER,
+    LABRADOR
+}
+
+public class Dog
+{
+    private readonly bool _isPottyTrained;
+    private readonly int _numberOfBones;
+    private readonly DogType _type;
+    private readonly double _litersOfWater;
+
+    public Dog(DogType type, int numberOfBones, double litersOfWater, bool isPottyTrained)
+    {
+        _type = type;
+        _numberOfBones = numberOfBones;
+        _litersOfWater = litersOfWater;
+        _isPottyTrained = isPottyTrained;
+    }
+
+    public double GetRunningSpeed()
+    {
+        switch (_type)
+        {
+            case DogType.POINTER:
+                return GetBaseRunningSpeed();
+            case DogType.RETRIEVER:
+                return Math.Max(0, GetBaseRunningSpeed() - (GetBoneFactor() * _numberOfBones));
+            case DogType.LABRADOR:
+                return _isPottyTrained ? 0 : GetBaseSpeed(_litersOfWater);
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private double GetBaseSpeed(double litersOfWater)
+    {
+        return Math.Min(24.0, litersOfWater * GetBaseRunningSpeed());
+    }
+
+    private double GetBoneFactor()
+    {
+        return 9.0;
+    }
+
+    private double GetBaseRunningSpeed()
+    {
+        return 12.0;
+    }
+
+    public string GetBark()
+    {
+        string value;
+        switch (_type)
+        {
+            case DogType.POINTER:
+                value = "Woof!";
+                break;
+            case DogType.RETRIEVER:
+                value = "Waaf!";
+                break;
+            case DogType.LABRADOR:
+                value = _litersOfWater > 0 ? "WOOF" : "-";
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        return value;
+    }
+}
---
+++ tests/UnitTests/ApplicationCore/Entities/DogTests/DogTests.cs
@@ -0,0 +1,90 @@
+﻿using Xunit;
+
+namespace Microsoft.eShopWeb.UnitTests.ApplicationCore.Entities.DogTests;
+
+public class DogTest
+{
+    [Fact]
+    public void GetRunningSpeedLabrador_PottyTrained()
+    {
+        var dog = new Dog(DogType.LABRADOR, 0, 0, true);
+        Assert.Equal(0.0, dog.GetRunningSpeed());
+    }
+
+    [Fact]
+    public void GetRunningSpeedLabrador_PottyTrained_With_Water()
+    {
+        var dog = new Dog(DogType.LABRADOR, 0, 1.5, true);
+        Assert.Equal(0.0, dog.GetRunningSpeed());
+    }
+
+    [Fact]
+    public void GetRunningSpeedLabrador_Not_PottyTrained()
+    {
+        var dog = new Dog(DogType.LABRADOR, 0, 1.5, false);
+        Assert.Equal(18.0, dog.GetRunningSpeed());
+    }
+
+    [Fact]
+    public void GetRunningSpeedLabrador_Not_PottyTrained_High_Water()
+    {
+        var dog = new Dog(DogType.LABRADOR, 0, 4, false);
+        Assert.Equal(24.0, dog.GetRunningSpeed());
+    }
+
+    [Fact]
+    public void GetRunningOfRetriever_With_No_Bones()
+    {
+        var dog = new Dog(DogType.RETRIEVER, 0, 0, false);
+        Assert.Equal(12.0, dog.GetRunningSpeed());
+    }
+
+    [Fact]
+    public void GetRunningOfRetriever_With_One_Bone()
+    {
+        var dog = new Dog(DogType.RETRIEVER, 1, 0, false);
+        Assert.Equal(3.0, dog.GetRunningSpeed());
+    }
+
+    [Fact]
+    public void GetRunningOfRetriever_With_Two_Bone()
+    {
+        var dog = new Dog(DogType.RETRIEVER, 2, 0, false);
+        Assert.Equal(0.0, dog.GetRunningSpeed());
+    }
+
+    [Fact]
+    public void GetRunningSpeedOfPointer()
+    {
+        var dog = new Dog(DogType.POINTER, 0, 0, false);
+        Assert.Equal(12.0, dog.GetRunningSpeed());
+    }
+
+    [Fact]
+    public void GetBarkOfPointer()
+    {
+        var dog = new Dog(DogType.POINTER, 0, 0, false);
+        Assert.Equal("Woof!", dog.GetBark());
+    }
+
+    [Fact]
+    public void GetBarkOfRetriever()
+    {
+        var dog = new Dog(DogType.RETRIEVER, 2, 0, false);
+        Assert.Equal("Waaf!", dog.GetBark());
+    }
+
+    [Fact]
+    public void GetBarkOfLabrador_High_Water()
+    {
+        var dog = new Dog(DogType.LABRADOR, 0, 4, false);
+        Assert.Equal("WOOF", dog.GetBark());
+    }
+
+    [Fact]
+    public void GetBarkOfLabrador_No_Water()
+    {
+        var dog = new Dog(DogType.LABRADOR, 0, 0, false);
+        Assert.Equal("-", dog.GetBark());
+    }
+}
