f205e30f6795d011f85919c23223b555ccedefff
jovaneyck <jo.vaneyck@mgail.com>
2025-07-08 09:43:16 +0200

    roll back repo changes, the aggregate includes are embedded in the specifications

--- tests/IntegrationTests/Repositories/BasketRepositoryTests.cs
+++ tests/IntegrationTests/Repositories/BasketRepositoryTests.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.ApplicationCore.Specifications;
 using Microsoft.eShopWeb.Infrastructure.Data;
 using Microsoft.eShopWeb.UnitTests.Builders;
 using Xunit;
@@ -11,12 +12,12 @@ namespace Microsoft.eShopWeb.IntegrationTests.Repositories;
 
 public class BasketRepositoryTests
 {
-    private (DbContext, BasketRepository repository) BuildFreshRepository()
+    private (DbContext, EfRepository<Basket> repository) BuildFreshRepository()
     {
         var context = new CatalogContext(new DbContextOptionsBuilder<CatalogContext>()
             .UseInMemoryDatabase(databaseName: "TestCatalog")
             .Options);
-        return (context, new BasketRepository(context));
+        return (context, new EfRepository<Basket>(context));
     }
 
     [Fact]
@@ -28,7 +29,9 @@ public class BasketRepositoryTests
         await writeContext.SaveChangesAsync(TestContext.Current.CancellationToken);
         
         var (_, repo) = BuildFreshRepository();
-        var retrieved = await repo.GetAggregateById(basket.Id, TestContext.Current.CancellationToken);
+        var retrieved = await repo.FirstOrDefaultAsync(
+            new BasketWithItemsSpecification(basket.Id), 
+            TestContext.Current.CancellationToken);
         
         Assert.NotNull(retrieved);
         Assert.Equal(basket.BuyerId, retrieved.BuyerId);
