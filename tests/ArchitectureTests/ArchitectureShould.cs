2bf0dcffba04e22b7c99530eda1c1e7f0360cdd4
Jo Van Eyck <jovaneyck1988@hotmail.com>
2026-01-09 11:02:40 +0100

    add archunit tests

---
+++ tests/ArchitectureTests/ArchitectureShould.cs
@@ -0,0 +1,69 @@
+﻿﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Loader;
+using ArchUnitNET.Fluent;
+using ArchUnitNET.xUnitV3;
+using Xunit;
+
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+using Assembly = System.Reflection.Assembly;
+
+namespace ArchitectureTests;
+
+public class ArchitectureShould
+{
+    private static Assembly _publicApiAssembly = Assembly.Load("PublicApi");
+    private static Assembly _applicationCoreAssembly = Assembly.Load("ApplicationCore");
+    private static Assembly _infrastructureAssembly = Assembly.Load("Infrastructure");
+    private static Assembly _efCoreAssembly = Assembly.Load("Microsoft.EntityFrameworkCore");
+
+    private static readonly Architecture Architecture = new ArchLoader() 
+        .LoadAssemblies(
+            _publicApiAssembly,
+            _applicationCoreAssembly,
+            _infrastructureAssembly,
+            _efCoreAssembly)
+        .Build();
+    
+    private readonly IObjectProvider<IType> PublicApi =
+        Types().That().ResideInAssembly(_publicApiAssembly).As("PublicApi");
+    
+    private readonly IObjectProvider<IType> ApplicationCore =
+        Types().That().ResideInAssembly(_applicationCoreAssembly).As("ApplicationCore");
+    
+    private readonly IObjectProvider<IType> Infrastructure =
+        Types().That().ResideInAssembly(_infrastructureAssembly).As("Infrastructure");
+
+    private readonly IObjectProvider<IType> EfCore =
+        Types().That().ResideInAssembly(_infrastructureAssembly).As("EfCore");
+
+    [Fact]
+    public void SanityCheck()
+    {
+        Assert.NotEmpty(ApplicationCore.GetObjects(Architecture));
+        Assert.NotEmpty(Infrastructure.GetObjects(Architecture));
+        Assert.NotEmpty(PublicApi.GetObjects(Architecture));
+        Assert.NotEmpty(EfCore.GetObjects(Architecture));
+    }
+    
+    [Fact]
+    public void CoreShouldNotDependOnInfrastructure()
+    {
+        Types().That()
+            .Are(ApplicationCore)
+            .Should()
+            .NotDependOnAny(Infrastructure)
+            .Because("DIP")
+            .Check(Architecture);
+    }
+    
+    [Fact]
+    public void CoreShouldNotDependOnEntityFramework()
+    {
+        Types().That()
+            .Are(ApplicationCore)
+            .Should()
+            .NotDependOnAny(EfCore)
+            .Because("DIP")
+            .Check(Architecture);
+    }
+}
